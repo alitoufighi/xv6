083d39c253f91f45c681bbff4622ac0d0cc7258f
Mohammad Ali Toufighi <alitoumob@gmail.com>
2018-12-19 16:14:21 +0330

    add random function for lottary scheduling

--- proc.c
+++ proc.c
@@ -20,6 +20,15 @@ extern void trapret(void);
 
 static void wakeup1(void *chan);
 
+static unsigned long X = 1; // used in pseudo-random generator
+
+// generates a pseudo-random number using LCG between 0 and M (inclusive)
+int rand(int M) {
+  unsigned long a = 1103515245, c = 12345;
+  X = a * X + c; 
+  return ((unsigned int)(X / 65536) % 32768) % M + 1;
+}
+
 void
 pinit(void)
 {
@@ -353,6 +362,15 @@ scheduler(void)
     release(&ptable.lock);
 
   }
+}
+
+
+void
+lottary_scheduler(void)
+{
+  int num_of_tickets = 480;
+  int ticket = rand(num_of_tickets);
+
 }
 
 // Enter scheduler.  Must hold only ptable.lock
