936d9e86e82abf224e1742f8194d124d689a7aa1
Mohammad Ali Toufighi <alitoumob@gmail.com>
2018-12-20 08:35:17 +0330

    refactoring fcfstest user program

--- fcfstest.c
+++ fcfstest.c
@@ -21,40 +21,22 @@ int main()
 		
 		else if (pid == 0)
 		{
-			// for (long int i = 0; i < 10000; i++)
-			printf(1, "Before Number is %d\n", getpid());
-			// 	for (long int j = 0; j < 10000; j++);
+			int random = rand(25000);
+			printf(1, "Before changing level: Process %d waiting for %d\n", getpid(), random);
+			for(int i = 0; i < random * 12345; ++i); // waiting randomly before changing level
 			change_level(2);
-			// for (long int i = 0; i < 10000; i++)
-			// 	for (long int j = 0; j < 10000; j++);
-			printf(1, "After Number is %d\n", getpid());
+			printf(1, "After  changing level: Process %d\n", getpid());
 			break;
 		}
 	}
 	
-	// if (pid < 0)
-	// {
-	// 	printf(2, "fork error\n");
-	// }
-	
-	// else if (pid == 0)
-	// {
-	// 	// for (long int i = 0; i < 10000; i++)
-	// 	// 	for (long int j = 0; j < 10000; j++);
-	// 	change_level(2);
-	// 	// for (long int i = 0; i < 10000; i++)
-	// 	// 	for (long int j = 0; j < 10000; j++);
-	// 	printf(1, "Number is %d\n", getpid());
-
-	// }
-
 	if(pid > 0)
 	{
-		// printf(1, "waiting\n");
+		printf(1, "Waiting for children\n");
 		for (int i = 0; i < NCHILD; i++)
 			wait();
 		
-		printf(1, "user program finished\n");
+		printf(1, "User program finished\n");
 	}
 
 	exit();
