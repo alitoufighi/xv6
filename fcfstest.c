1742cba3c9b0f90452ede67640ce1925cab9cc94
Mohammad Ali Toufighi <alitoumob@gmail.com>
2018-12-20 07:42:25 +0330

    change fcfs user program test

--- fcfstest.c
+++ fcfstest.c
@@ -10,27 +10,45 @@ int main()
 
 	change_level(2);
 
-	for (int i = 0; i < NCHILD; i++)
+	for (int i = 0; i < NCHILD; i++){
 		if (pid > 0)
 			pid = fork();
-	
-	if (pid < 0)
-	{
-		printf(2, "fork error\n");
+
+		if (pid < 0)
+		{
+			printf(2, "fork error\n");
+		}
+		
+		else if (pid == 0)
+		{
+			// for (long int i = 0; i < 10000; i++)
+			printf(1, "Before Number is %d\n", getpid());
+			// 	for (long int j = 0; j < 10000; j++);
+			change_level(2);
+			// for (long int i = 0; i < 10000; i++)
+			// 	for (long int j = 0; j < 10000; j++);
+			printf(1, "After Number is %d\n", getpid());
+			break;
+		}
 	}
 	
-	else if (pid == 0)
-	{
-		for (long int i = 0; i < 10000; i++)
-			for (long int j = 0; j < 10000; j++);
-		change_level(2);
-		for (long int i = 0; i < 10000; i++)
-			for (long int j = 0; j < 10000; j++);
-		printf(1, "Number is %d\n", getpid());
+	// if (pid < 0)
+	// {
+	// 	printf(2, "fork error\n");
+	// }
+	
+	// else if (pid == 0)
+	// {
+	// 	// for (long int i = 0; i < 10000; i++)
+	// 	// 	for (long int j = 0; j < 10000; j++);
+	// 	change_level(2);
+	// 	// for (long int i = 0; i < 10000; i++)
+	// 	// 	for (long int j = 0; j < 10000; j++);
+	// 	printf(1, "Number is %d\n", getpid());
 
-	}
+	// }
 
-	else
+	if(pid > 0)
 	{
 		// printf(1, "waiting\n");
 		for (int i = 0; i < NCHILD; i++)
