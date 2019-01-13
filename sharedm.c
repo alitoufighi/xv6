8f294a533faa963f44c324878ee99ee4a47a1f1f
felagund <zangeneh.saeed411@yahoo.com>
2019-01-13 18:28:04 +0330

    temp

--- sharedm.c
+++ sharedm.c
@@ -1,16 +1,15 @@
 #include "sharedm.h"
 #include "proc.h"
 #include "syscall.h"
+// #include "vm.c"
 
 struct shm_info* find_shm_info(int id){
-	acquire(&shm_table.lock);
 	struct shm_info* info = 0x0000;
 	for(int i = 0; i < SHM_COUNT; ++i){
 		*info = shm_table.shm_information[i];
 		if(info->id == id)
 			break;
 	}
-	release(&shm_table.lock);
 
 	return info;
 }
@@ -18,6 +17,11 @@ struct shm_info* find_shm_info(int id){
 int check_id(int id){
 	struct shm_info* info = find_shm_info(id);
 	return (int)info;
+}
+
+int has_permission(struct shm_info* info)
+{
+	return 1;
 }
 
 int check_flag(int flag){
@@ -33,9 +37,6 @@ int sys_shm_open(void)
 {
 	int id, pgcount, flag;
 	if (argint(0, &id) < 0)
-		return -1;
-	
-	if (check_id(id))
 		return -1;
 
 	if (argint(1, &pgcount) < 0)
@@ -53,6 +54,12 @@ int sys_shm_open(void)
 
 	acquire(&shm_table.lock);
 
+	if (check_id(id))
+	{
+		release(&shm_table.lock);
+		return -1;
+	}
+
 	struct shm_info info = shm_table.shm_information[id];
 	info.owner_pid = myproc()->pid;
 	info.flags = flag;
@@ -61,12 +68,11 @@ int sys_shm_open(void)
 	uint* frame;
 	for (int i = 0; i < pgcount; ++i) {
 		if((frame = kalloc()) == 0){
+			release(&shm_table.lock);
 			return -1;
 		}
 		info.frame[i] = frame;
 	}
-
-	release(&shm_table.lock);
 
 	return 1;
 }
@@ -82,9 +88,26 @@ int sys_shm_attach(void)
 		return -1;
 	}
 
-	struct shm_info info = shm_table.shm_information[id];
-	info.refcnt++; 
-	
+	acquire(&shm_table.lock);
+
+	struct shm_info* info = find_shm_info(id);
+
+	if (!has_permission(info))
+	{
+		release(&shm_table.lock);
+		return -1;
+	}
+
+	info->refcnt++; 
+	int index;
+
+	for (index = 0; index < info->size; index++)
+	{
+		// if (mappages)
+	}
+
+	release(&shm_table.lock);
+
 	return 0x00000;
 }
 
