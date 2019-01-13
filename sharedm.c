cead6a0fe5ed00f9908117ae04cf89e6344c1bc4
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-01-13 17:37:43 +0330

    fetch arg in shm_attach

--- sharedm.c
+++ sharedm.c
@@ -1,6 +1,13 @@
 #include "sharedm.h"
 #include "syscall.h"
 
+int check_id(int id){
+	if(id < 0 || id >= SHM_COUNT){
+		return -1;
+	}
+	return 0;
+}
+
 int sys_shm_open(void)
 {
 	return 0;
@@ -8,6 +15,18 @@ int sys_shm_open(void)
 
 int sys_shm_attach(void)
 {
+	int id;
+	if(argint(0, &id) < 0){
+		return -1;
+	}
+
+	if(check_id(id)){
+		return -1;
+	}
+
+	struct shm_info info = shm_table.shm_information[id];
+	info.refcnt++; 
+	
 	return 0x00000;
 }
 
